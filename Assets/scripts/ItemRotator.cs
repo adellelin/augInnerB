84146048ceb2196617d0409e21760e47d33dbe16
adellelin <adelle.lin@gmail.com>
2016-04-15 11:52:50 -0400

    first commit

---
+++ Assets/scripts/ItemRotator.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ItemRotator : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// time.deltatime changes the change from framrate to time
+		transform.Rotate (new Vector3 (15, 30, 45) * Time.deltaTime);
+	}
+
+	// void fixedUpdate () {} is a function that uses time
+}
