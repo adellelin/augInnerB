f1249bb5da418c6929062070ba6783be0df8a57d
adellelin <adelle.lin@gmail.com>
2016-04-19 21:50:31 -0400

    animations and exploding helmet

---
+++ Assets/scripts/destructionController.js
@@ -0,0 +1,18 @@
+﻿#pragma strict
+
+function Start () {
+
+}
+
+public var chainPrefab: GameObject;
+
+function Update()
+ {
+ 
+ 
+     if(Input.GetKeyDown("space"))
+     {
+     	 Instantiate(chainPrefab, transform.position, transform.rotation); 
+         Destroy(gameObject);
+     }
+ }
